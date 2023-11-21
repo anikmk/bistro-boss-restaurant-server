6cf151dbb8b8189479bf8601e42821df6df16e28
anikmk <anikmollik122321@gmail.com>
2023-11-21 20:56:37 +0600

    make status code

--- index.js
+++ index.js
@@ -40,9 +40,23 @@ async function run() {
       const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'});
       res.send({token});
     })
-
+    //  middleware verify Token
+    const verifyToken = (req,res,next) => {
+      console.log('inside verify token',req.headers.authorization);
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'forbidden access'});
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        if(err){
+          return res.status(401).send({message:'fobidden acoount'})
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
     // user releted api
-    app.get('/users',async(req,res)=>{
+    app.get('/users', verifyToken ,async(req,res)=>{
       const result = await userCollection.find().toArray();
       res.send(result);
 
