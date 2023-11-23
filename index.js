8d446a98d48414919b412741ffeaeec39e51db83
anikmk <anikmollik122321@gmail.com>
2023-11-23 11:49:02 +0600

    make update api

--- index.js
+++ index.js
@@ -122,13 +122,43 @@ async function run() {
       const result = await menuCollection.find().toArray();
       res.send(result);
     })
+    app.get('/menu/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    })
 
     app.post('/menu', verifyToken,verifyAdmin, async(req,res)=>{
       const item = req.body;
       const result = await menuCollection.insertOne(item);
       res.send(result);
     })
+    app.patch('/menu/:id',async(req,res)=>{
+      const item = req.body;
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set:{
+          name: item.name,
+          category: item.category,
+          price:item.price,
+          recipe:item.recipe,
+          image:item.image
+        }
+      }
+      const result = await menuCollection.updateOne(filter,updatedDoc);
+      res.send(result);
+    })
 
+    app.delete('/menu/:id', verifyToken,verifyAdmin, async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // reviews releted api
     app.get('/reviews',async(req,res)=>{
       const result = await reviewsCollection.find().toArray();
       res.send(result);
